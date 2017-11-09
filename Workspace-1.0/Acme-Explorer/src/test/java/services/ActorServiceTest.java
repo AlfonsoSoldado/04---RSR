d74cddcab08029dd2b573506f007b678a9cfbaa6
marmargal <inma.martin.galvan@hotmail.com>
2017-11-09 14:12:44 +0100

    Test (sin terminar) de actor

---
+++ Workspace-1.0/Acme-Explorer/src/test/java/services/ActorServiceTest.java
@@ -0,0 +1,27 @@
+package services;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+				"classpath:spring/datasource.xml",
+				"classpath:spring/config/packages.xml"})
+@Transactional
+public class ActorServiceTest extends AbstractTest{
+	
+	// Serice under test -------------------------
+	
+	@Autowired
+	private ActorService actorService;
+	
+	// Test --------------------------------------
+	
+
+}
