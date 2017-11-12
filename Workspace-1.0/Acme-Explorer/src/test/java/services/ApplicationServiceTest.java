ad581bff88f72c15d02db7e33a29c92e22679b31
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2017-11-12 16:19:16 +0100

    Commit Application

--- Workspace-1.0/Acme-Explorer/src/test/java/services/ApplicationServiceTest.java
+++ Workspace-1.0/Acme-Explorer/src/test/java/services/ApplicationServiceTest.java
@@ -1,11 +1,18 @@
 package services;
 
+import java.util.Collection;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Application;
+import domain.Curriculum;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -14,4 +21,30 @@ import utilities.AbstractTest;
 @Transactional
 public class ApplicationServiceTest extends AbstractTest {
 
+	// Service under test -------------------------
+	
+	@Autowired
+	private ApplicationService applicationService;
+			
+	// Supporting services -----------------------
+			
+			
+	// Test --------------------------------------
+	
+	@Test
+	public void testCreateApplication() {
+		authenticate("explorer01");
+		Application application;
+		application = this.applicationService.create();
+		Assert.notNull(application);
+		unauthenticate();
+	}
+	
+	@Test
+	public void testFindAllApplication() {
+		authenticate("explorer01");
+		Collection<Application> applications;
+		applications = this.applicationService.findAll();
+		Assert.notNull(applications);
+	}
 }
