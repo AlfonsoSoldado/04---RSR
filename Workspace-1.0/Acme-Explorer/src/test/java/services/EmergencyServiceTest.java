4a6d0018fd4e82039b06fdd7124fa03fb5c88b8d
marmargal <inma.martin.galvan@hotmail.com>
2017-11-13 11:35:54 +0100

    EmergencyTest y cambios tontos

--- Workspace-1.0/Acme-Explorer/src/test/java/services/EmergencyServiceTest.java
+++ Workspace-1.0/Acme-Explorer/src/test/java/services/EmergencyServiceTest.java
@@ -1,9 +1,17 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Emergency;
 
 import utilities.AbstractTest;
 
@@ -13,5 +21,57 @@ import utilities.AbstractTest;
 				"classpath:spring/config/packages.xml"})
 @Transactional
 public class EmergencyServiceTest extends AbstractTest {
-
+	
+	// Service under test -----------------
+	@Autowired
+	private EmergencyService emergencyService;
+	
+	// Supporting service -----------------
+	
+	
+	// Test -------------------------------
+	
+	@Test
+	public void testCreateEmergency(){
+		Emergency emergency;
+		emergency = this.emergencyService.create();
+		Assert.notNull(emergency);
+	}
+	
+	@Test
+	public void testFindAllEmergency(){
+		Collection<Emergency> emergencies;
+		emergencies = new ArrayList<Emergency>();
+		emergencies = this.emergencyService.findAll();
+		Assert.notNull(emergencies);		
+	}
+	
+	@Test
+	public void testFindOneEmergency(){
+		Emergency emergency;
+		emergency = this.emergencyService.findOne(super.getEntityId("emergency1"));
+		Assert.notNull(emergency);
+	}
+	
+	@Test
+	public void testSaveEmergency(){
+		Emergency emergency;
+		emergency = this.emergencyService.create();
+		
+		emergency.setName("SampleEmergency");
+		
+		emergency.setEmail("sample@gmail.com");
+		
+		emergency.setPhoneNumber("654321234");
+		
+	}
+	
+	@Test
+	public void testDeleteEmergency(){
+		Emergency emergency;
+		emergency = this.emergencyService.findOne(super.getEntityId("emergency1"));
+		this.emergencyService.delete(emergency);
+	}
+	
+	
 }
