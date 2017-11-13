5145eb4e6671fcb6870e14805dd500c99a05d813
Mario <codec993@gmail.com>
2017-11-13 11:36:53 +0100

    Repository

--- Workspace-1.0/Acme-Explorer/src/main/java/repositories/MessageRepository.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/repositories/MessageRepository.java
@@ -1,11 +1,18 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import domain.Actor;
 import domain.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>{
 	
+	@Query("select a from Actor a, Message m where a=m.sender")
+	Actor senderActor();
+	
+	@Query("select a from Actor a, Message m where a=m.recipient")
+	Actor recipientActor();
 }
