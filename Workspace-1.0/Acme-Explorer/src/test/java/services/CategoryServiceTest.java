32423c46e9ad7edbaf5f2495cf5fdfeaf44891ee
marmargal <inma.martin.galvan@hotmail.com>
2017-11-13 10:30:06 +0100

    Sin terminar

--- Workspace-1.0/Acme-Explorer/src/test/java/services/CategoryServiceTest.java
+++ Workspace-1.0/Acme-Explorer/src/test/java/services/CategoryServiceTest.java
@@ -1,9 +1,16 @@
 package services;
 
+import java.util.Collection;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Category;
 
 import utilities.AbstractTest;
 
@@ -13,5 +20,44 @@ import utilities.AbstractTest;
 				"classpath:spring/config/packages.xml"})
 @Transactional
 public class CategoryServiceTest extends AbstractTest{
-
+	
+	// Service under test -------------------
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	// Supporting services ----------------
+	
+	
+	// Test -------------------------------
+	
+	@Test 
+	public void testCreateCategory(){
+		Category category;
+		category = this.categoryService.create();
+		Assert.notNull(category);
+	}
+	
+	@Test
+	public void testFindAllCategory(){
+		Collection<Category> categories;
+		categories = this.categoryService.findAll();
+		Assert.notNull(categories);
+	}
+	
+	@Test
+	public void testFindOneCategory(){
+		Category category;
+		category = this.categoryService.findOne(super.getEntityId("category1"));
+		Assert.notNull(category);
+	}
+	
+	@Test
+	public void testSaveCategory(){
+		
+	}
+	
+	
+	
+	
 }
