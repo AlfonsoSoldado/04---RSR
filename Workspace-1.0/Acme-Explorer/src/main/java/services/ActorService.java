eedbc10cd95b5e8691c14aadd1c4c556fbab90c7
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2017-11-14 20:35:20 +0100

    Actor Send, Move, Delete messages

--- Workspace-1.0/Acme-Explorer/src/main/java/services/ActorService.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/services/ActorService.java
@@ -36,16 +36,16 @@ public class ActorService {
 
 	@Autowired
 	private ActorRepository actorRepository;
-	
+
 	@Autowired
 	private RangerRepository rangerRepository;
-	
+
 	@Autowired
 	private ManagerRepository managerRepository;
-	
+
 	@Autowired
 	private ConfigurationRepository configurationRepository;
-	
+
 	@Autowired
 	private MessageRepository messageRepository;
 
@@ -53,10 +53,10 @@ public class ActorService {
 
 	@Autowired
 	private UserAccountService userAccountService;
-	
+
 	@Autowired
 	private TripService tripService;
-	
+
 	@Autowired
 	private AdministratorService administratorService;
 
@@ -98,7 +98,7 @@ public class ActorService {
 	}
 
 	// Other business methods
-	
+
 	public void checkUserLogin() {
 		UserAccount userAccount = LoginService.getPrincipal();
 		Assert.notNull(userAccount);
@@ -115,7 +115,7 @@ public class ActorService {
 		Assert.notNull(res);
 		return res;
 	}
-	
+
 	public boolean checkAuthority() {
 		boolean result = false;
 		UserAccount userAccount;
@@ -150,63 +150,63 @@ public class ActorService {
 	}
 
 	// 10.4
-	
+
 	public Collection<Trip> searchTripsByCategory(Category category) {
 		Collection<Trip> res;
 		res = this.tripService.findTripsByCategory(category);
 		return res;
 	}
 
-	//30.1
+	// 30.1
 	public Collection<Curriculum> findCurriculumRangerByTrip(int id) {
 		Collection<Curriculum> res = new ArrayList<Curriculum>();
-		
+
 		// Añadimos todos los Curriculum mediante la query.
-		
+
 		res.addAll(actorRepository.findCurriculumRangerByTrip(id));
 		Assert.notNull(res);
 		return res;
 	}
-	
-	//30.2
-	public Collection<Audit> findAuditByTrip(int id){
+
+	// 30.2
+	public Collection<Audit> findAuditByTrip(int id) {
 		Collection<Audit> res = new ArrayList<Audit>();
-		
+
 		// Añadimos todos los Audit mediante la query
-		
+
 		res.addAll(actorRepository.findAuditsByTrip(id));
 		Assert.notNull(res);
 		return res;
 
 	}
-	
+
 	// 35.2
-	public void banActor(Actor actor){
+	public void banActor(Actor actor) {
 		administratorService.checkAuthority();
 		Collection<Actor> res = new ArrayList<Actor>();
 		res.addAll(rangerRepository.banRanger());
 		res.addAll(managerRepository.banManager());
-		if(res.contains(actor)){
+		if (res.contains(actor)) {
 			Collection<Authority> authorities = new ArrayList<Authority>();
 			authorities.addAll(actor.getUserAccount().getAuthorities());
-			for(Authority a: authorities){
+			for (Authority a : authorities) {
 				actor.getUserAccount().removeAuthority(a);
 			}
 		}
 	}
-	
+
 	// 35.3
-	public void unbanActor(Actor actor){
+	public void unbanActor(Actor actor) {
 		administratorService.checkAuthority();
 		Collection<Actor> res = new ArrayList<Actor>();
 		res.addAll(rangerRepository.unbanRanger());
 		res.addAll(managerRepository.unbanManager());
-		if(res.contains(actor)){
-			if(actor.getClass().equals(Ranger.class)){
+		if (res.contains(actor)) {
+			if (actor.getClass().equals(Ranger.class)) {
 				Authority auth = new Authority();
 				auth.setAuthority("RANGER");
 				actor.getUserAccount().addAuthority(auth);
-			} else if(actor.getClass().equals(Manager.class)) {
+			} else if (actor.getClass().equals(Manager.class)) {
 				Authority auth2 = new Authority();
 				auth2.setAuthority("MANAGER");
 				actor.getUserAccount().addAuthority(auth2);
@@ -232,111 +232,168 @@ public class ActorService {
 			messages.clear();
 		}
 	}
+
 	public void checkSpamWords() {
 		administratorService.checkAuthority();
 		Collection<String> spamWords = new ArrayList<String>();
 		spamWords = configurationRepository.findSpamWords();
-		
+
 		Collection<Message> messages = new ArrayList<Message>();
 		messages = messageRepository.findMessages();
-		
-		for(String s: spamWords){
-			for(Message m: messages){
-				if(m.getBody().contains(s) || m.getSubject().contains(s)){
+
+		for (String s : spamWords) {
+			for (Message m : messages) {
+				if (m.getBody().contains(s) || m.getSubject().contains(s)) {
 					m.setSpam(true);
 				}
 			}
 		}
 	}
-	
-	public void actorToSuspiciousList(){
+
+	public void actorToSuspiciousList() {
 		administratorService.checkAuthority();
-		
+
 		checkSpamWords();
-		
+
 		ListSuspicious ls = new ListSuspicious();
-		
+
 		Collection<Actor> actors = new ArrayList<Actor>();
-		
+
 		Collection<Message> rangerMessages = new ArrayList<Message>();
 		rangerMessages = messageRepository.findRangerMessages();
-		
+
 		Collection<Message> managerMessages = new ArrayList<Message>();
 		managerMessages = messageRepository.findManagerMessages();
-		
-		for(Message m: rangerMessages){
+
+		for (Message m : rangerMessages) {
 			Ranger r;
 			r = (Ranger) m.getSender();
-			
-			if(r.getSuspicious() == false){
+
+			if (r.getSuspicious() == false) {
 				r.setSuspicious(true);
 			}
-			
-			
+
 			actors = ls.getSuspicious();
 			actors.add(r);
-			
+
 			ls.setSuspicious(actors);
-			
+
 			actors.clear();
 		}
-		
-		for(Message m: managerMessages){
+
+		for (Message m : managerMessages) {
 			Manager ma;
 			ma = (Manager) m.getSender();
-			
-			if(ma.getSuspicious()==false){
+
+			if (ma.getSuspicious() == false) {
 				ma.setSuspicious(true);
 			}
-			
+
 			actors = ls.getSuspicious();
 			actors.add(ma);
-			
+
 			ls.setSuspicious(actors);
-			
+
 			actors.clear();
 		}
 	}
+
+	public void sendMessage(Collection<Actor> recipients, Actor sender,
+			Message message) {
+		Assert.isTrue(checkAuthority());
+
+		checkSpamWord(message);
+		Boolean isSpam = message.getSpam();
+		
+		Collection<Message> messages = new ArrayList<Message>();
+		
+		for(Actor a: recipients){
+			for(Folder f: a.getFolders()){
+				if(isSpam && f.getName().equals("Spam")){
+					messages.addAll(f.getMessages());
+					messages.add(message);
+					
+					f.setMessages(messages);
+					
+					messages.clear();
+				} else if(isSpam == false && f.getName().equals("In Box")) {
+					messages.addAll(f.getMessages());
+					messages.add(message);
+					
+					f.setMessages(messages);
+					
+					messages.clear();
+				}
+			}
+		}
+
+		for(Folder f: sender.getFolders()){
+			if(f.getName().equals("Out Box")){
+				messages.addAll(f.getMessages());
+				messages.add(message);
+				
+				f.setMessages(messages);
+				
+				messages.clear();
+			}
+		}
+	}
+
+	private void checkSpamWord(Message message) {
+		Collection<String> spamWords = new ArrayList<String>();
+		spamWords = configurationRepository.findSpamWords();
+
+		for (String s : spamWords) {
+			if (message.getBody().contains(s) || message.getSubject().contains(s)) {
+				message.setSpam(true);
+			}
+		}
+	}
+	
+	public void deleteMessage(Actor actor, Message message){
+		checkAuthority();
+		Collection<Message> messages = new ArrayList<Message>();
+		
+		for(Folder f: actor.getFolders()){
+			if(f.getName().equals("Trash") && !f.getMessages().contains(message)){
+				messages.addAll(f.getMessages());
+				messages.add(message);
+				
+				f.setMessages(messages);
+				
+				messages.clear();
+			} else if(f.getName().equals("Trash") && f.getMessages().contains(message)){
+				messages.addAll(f.getMessages());
+				messages.remove(message);
+				
+				f.setMessages(messages);
+				
+				messages.clear();
+			}
+		}
+	}
+	
+	public void moveMessage(Message message, Actor actor, Folder folder){
+		checkAuthority();
+		
+		Collection<Message> messages = new ArrayList<Message>();
+		
+		for(Folder f: actor.getFolders()){
+			if(!message.getFolder().contains(folder)){
+				messages.addAll(f.getMessages());
+				messages.add(message);
+				
+				folder.setMessages(messages);
+				
+				messages.clear();
+				
+				messages.addAll(f.getMessages());
+				messages.remove(message);
+				
+				f.setMessages(messages);
+				
+				messages.clear();
+			}
+		}
+	}
 }
-	
-	
-//	public void sendMessage(Message message, Actor sender, Actor receiver) {
-//
-//		Assert.notNull(message);
-//		this.checkUserLogin();
-//
-//		UserAccount userAccount;
-//		Message savedMessage, savedMessageCopy;
-//		Message messageReceiver;
-//		Message messageSender;
-//
-//		userAccount = LoginService.getPrincipal();
-//		Assert.notNull(userAccount);
-//		Assert.notNull(sender);
-//
-//		Message messageCopy = this.messageService.copyMessage(message);
-//
-//		if (this.checkSpamWords(messageCopy))
-//			messageReceiver.setSpam(true);
-//			receiver.setFolders(Folders)
-//			
-//					
-//		else
-//			messageReceiver = (MessageFolder) receiver.getRe()
-//					.toArray()[4];
-//
-//		messageCopy.setMessageFolder(messageReceiver);
-//
-//		savedMessage = this.messageService.save(message);
-//		savedMessageCopy = this.messageService.save(messageCopy);
-//
-//		messageSender = message.getMessageFolder();
-//
-//		messageReceiver.getMessages().add(savedMessageCopy);
-//		messageSender.getMessages().add(savedMessage);
-//
-//		this.messageFolderService.save(messageSender);
-//		this.messageFolderService.save(messageReceiver);
-//
-//	}
-	
