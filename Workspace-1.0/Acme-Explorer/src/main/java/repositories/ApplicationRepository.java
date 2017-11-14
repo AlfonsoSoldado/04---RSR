d95659eecb2e009371e13711af97a16c8d6a9d17
danirguez <32953186+danirguez@users.noreply.github.com>
2017-11-14 11:51:08 +0100

    Application

--- Workspace-1.0/Acme-Explorer/src/main/java/repositories/ApplicationRepository.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/repositories/ApplicationRepository.java
@@ -24,4 +24,8 @@ public interface ApplicationRepository extends JpaRepository<Application, Intege
 	@Query("select a from Manager m join m.application a where m.id = ?1")
 	Collection<Application> findListApplication(int id);
 	
+	// 12.2
+	@Query("select a from Manager m join m.application a where m.id = ?1 and a.status like PENDING")
+	Collection<Application> findListApplicationPending(int id);
+	
 }
