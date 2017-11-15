10c28c2894fa66cb0e32865978ae5b8df7616e84
marmargal <inma.martin.galvan@hotmail.com>
2017-11-15 11:20:04 +0100

    Tests

--- Workspace-1.0/Acme-Explorer/src/test/java/services/ConfigurationServiceTest.java
+++ Workspace-1.0/Acme-Explorer/src/test/java/services/ConfigurationServiceTest.java
@@ -1,9 +1,17 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Configuration;
 
 import utilities.AbstractTest;
 
@@ -14,4 +22,80 @@ import utilities.AbstractTest;
 @Transactional
 public class ConfigurationServiceTest extends AbstractTest{
 
+	// Service under test --------------
+	
+	@Autowired
+	private ConfigurationService configurationService;
+	
+	
+	// Supporting services -------------
+	
+	// Test ----------------------------
+	
+	@Test
+	public void testCreateConfiguration(){
+		Configuration conf;
+		conf = this.configurationService.create();
+		Assert.notNull(conf);
+	}
+	
+	@Test
+	public void testFindOneConfiguration(){
+		Configuration conf;
+		conf = this.configurationService.findOne(super.getEntityId("configuration1"));
+		Assert.notNull(conf);
+	}
+	
+	@Test
+	public void testFindAllConfiguration(){
+		Collection<Configuration> conf;
+		conf = new ArrayList<Configuration>();
+		conf = this.configurationService.findAll();
+		Assert.notNull(conf);
+	}
+	
+	@Test
+	public void testSaveConfiguration(){
+		Configuration conf;
+		Double tax;
+		String banner, message, tag, countryCode,category, text, oth;
+		Collection<String> catalogueTag, treeCategory, catalogueText, other;
+	
+		conf = this.configurationService.create();
+		catalogueTag = new ArrayList<String>();
+		treeCategory = new ArrayList<String>();
+		catalogueText = new ArrayList<String>();
+		other = new ArrayList<String>();
+		banner = "http://creek-tours.com/wp-content/uploads/Kenya-Tanzania-Family-Safari-banner.jpg" ;
+		message = "Tanzanika is an adven-ture company that ...";
+		tax = 0.22;
+		countryCode = "+34";
+		tag = "country";
+		catalogueTag.add(tag);
+		category = "CATEGORY";
+		treeCategory.add(category);
+		text = "Text 1";
+		catalogueText.add(text);
+		oth = "Other";
+		other.add(oth);
+		
+		conf.setBanner(banner);
+		conf.setMessage(message);
+		//conf.setSpamWords(spamWords)
+		conf.setTax(tax);
+		conf.setCountryCode(countryCode);
+		conf.setCatalogueTag(catalogueTag);
+		conf.setTreeCategory(treeCategory);
+		conf.setCatalogueText(catalogueText);
+		conf.setOther(other);
+		
+		conf = this.configurationService.create();
+	}
+	
+	@Test 
+	public void testDeleteConfiguration(){
+		Configuration conf;
+		conf = this.configurationService.findOne(super.getEntityId("configuration1"));
+		this.configurationService.delete(conf);
+	}
 }
